24a1116fe7f7ed9be28b31021dd6af4f1412bcfc
Panda22082002 <druzhkova02@list.ru>
2018-10-14 21:06:44 +0300

    First commit

---
+++ Source4.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+int main() {
+	int x, n;
+	cin » x » n;
+	int res = x % n;
+	for (int count = 1; count < n; ++count) {
+		if (res * count % n == 1) {
+			cout « count;
+			return 0;
+		}
+	}
+	cout « "None";
+	return 0;
