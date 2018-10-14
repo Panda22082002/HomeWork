f5a2bdb007d7c7f3bcc51c52e398621cf8926dbb
Panda22082002 <43823367+Panda22082002@users.noreply.github.com>
2018-10-14 22:35:14 +0300

    Update Source4.cpp

--- 14.10.2018/Source4.cpp
+++ 14.10.2018/Source4.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main() {
 	int x, n;
-	cin » x » n;
+	cin >> x >> n;
 	int res = x % n;
 	for (int count = 1; count < n; ++count) {
 		if (res * count % n == 1) {
-			cout « count;
+			cout << count;
 			return 0;
 		}
 	}
-	cout « "None";
+	cout << "None";
 	return 0;
